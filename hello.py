2aaf3bfd24d6f8c4e0963ad1f08533840b04902a
kaorr <kaorufwonder@gmail.com>
2018-01-03 22:52:39 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self, username):
+        self.username = username
+
+
+@app.route("/")
+def top():
+    user_list = User.query.all()
+    return render_template('top.html', title='ユーザ一覧', user_list=user_list)
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    username = request.form.get('username')
+    if username:
+        user = User(username)
+        db.session.add(user)
+        db.session.commit()
+
+    return redirect(url_for('top'))
+
+@app.route("/user/<int:user_id>", methods=['GET'])
+def show_user(user_id):
+    target_user = User.query.get(user_id)
+
+    return render_template('show.html', title='ユーザ詳細', target_user=target_user)
+
+@app.route("/user/<int:user_id>", methods=['POST'])
+def mod_user(user_id):
+    target_user = User.query.get(user_id)
+    username = request.form.get('username')
+    if target_user and username:
+        target_user.username = username
+        db.session.commit()
+
+    return redirect(url_for('top'))
+
+@app.route("/del_user/<int:user_id>", methods=['POST'])
+def del_user(user_id):
+    target_user = User.query.get(user_id)
+    if target_user:
+        db.session.delete(target_user)
+        db.session.commit()
+
+    return redirect(url_for('top'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
